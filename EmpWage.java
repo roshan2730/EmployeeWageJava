3efa32965c6fe1ec96873abfe9b94798e068ac8c
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-05 15:31:32 +0530

    uc3:Calulating part time of employee

---
+++ EmpWage.java
@@ -0,0 +1,23 @@
+public class EmpWage {
+
+	public static void main(String[] args) {
+
+		//Constants		
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage : " +empWage);
+	}
+}
